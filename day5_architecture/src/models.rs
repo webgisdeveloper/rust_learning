7d21c963e57870229f43bafbdf74d557c8c2eadf
Jun Wang <webgisdeveloper@users.noreply.github.com>
2026-06-25 09:41:35 -0400

    Add private salary field to UserProfile with constructor

--- day5_architecture/src/models.rs
+++ day5_architecture/src/models.rs
@@ -2,5 +2,12 @@
 #[derive(Debug)]
 pub struct UserProfile {
     pub username: String,
-    pub role: String
+    pub role: String,
+    salary: f64,
+}
+
+impl UserProfile {
+    pub fn new(username: String, role: String, salary: f64) -> Self {
+        Self { username, role, salary }
+    }
 }
